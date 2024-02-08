ed6aa9f15715171899f4934b0d134121bb3b3d2f
nick lucas <nick@nickthedev.com>
2024-02-07 23:30:17 -1000

    Add LLM service to and update endpoint

--- backend/apps/prompt/services/tools.py
+++ backend/apps/prompt/services/tools.py
@@ -5,9 +5,9 @@ from langchain.tools import BaseTool, StructuredTool, tool
 class ShipDetailsInput(BaseModel):
     ship_id: str = Field(description="should be the id or identifier of a ship or vessel")
 
-
+# TODO: For testing purposes
 @tool("ship_details", args_schema=ShipDetailsInput, return_direct=True)
 def ship_details(ship_id: str) -> str:
     """Use this tool to look up details about a ship"""
-    print('ship_id:', ship_id)
+
     return "Ship 34k3j43k33 is in the sothern hemisphere"
